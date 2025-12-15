51a5f69c263621eda409638f39f131874c204f04
Your Name <you@example.com>
2025-12-15 11:28:39 +0000

    d

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-if [ "$(sha256sum "$1" | cut -d' ' -f1)" = "$2" ]; then echo "$1: OK"; else echo "$1: FAILED"; fi
+if test "$(sha256sum "$1" | cut -d' ' -f1)" = "$2"
+then
+echo "$1: OK"
+else
+echo "$1: FAILED"
+fi
