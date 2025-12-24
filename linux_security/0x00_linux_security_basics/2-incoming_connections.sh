b8785a0c30632d421abfc85ca9075f86f3be17ba
Your Name <you@example.com>
2025-12-24 11:08:12 +0000

    sss

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated"
-ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated (v6)"
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated"
+sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated (v6)"
